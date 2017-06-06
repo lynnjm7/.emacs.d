9639e00b9641201384168de14aa30ad0831c3c64
Josh Lynn <lynnjm7@gmail.com>
2017-06-06 00:19:25 -0700

    Commented out some unused package repos

--- init.el
+++ init.el
@@ -20,16 +20,17 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 
 (add-to-list 'package-archives
-	     '("tromey" . "http://tromey.com/elpa/") t)
-
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
-
-(add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
 
 (add-to-list 'package-archives
 	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
+;; (add-to-list 'package-archives
+;;	     '("tromey" . "http://tromey.com/elpa/") t)
+
+;;(add-to-list 'package-archives
+;;	     '("org" . "http://orgmode.org/elpa/") t)
+
 
 ;; Load packages 
 (package-initialize)
