0ceb2e0b1a294a82a28a777b24cb878963a13f01
Josh Lynn <lynnjm7@gmail.com>
2017-06-06 00:57:15 -0700

    Removed marmalade repos

--- init.el
+++ init.el
@@ -17,9 +17,6 @@
 ;; Package Repositories
 (require 'package)
 (add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
-
-(add-to-list 'package-archives
 	     '("melpa" . "http://melpa.org/packages/") t)
 
 (add-to-list 'package-archives
